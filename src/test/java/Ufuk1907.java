1e65983d650a2d1e591e1a06e1c76ddf3fc95696
ufukleipzig <ufuklaos@hotmail.com>
2020-10-13 23:17:20 +0200

    updated pom.xml

--- src/test/java/Ufuk1907.java
+++ src/test/java/Ufuk1907.java
@@ -2,8 +2,10 @@ public class Ufuk1907 {
 
     public static void main(String[] args) {
 
-        System.out.println("Hi Group14 Members! Let's make some changes! ");
+        System.out.println("Hi Group14 Members! Let's make some changes!");
+
 
     }
-
 }
+
+
