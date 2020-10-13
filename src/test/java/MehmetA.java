caa9f899922dd2cf62f4dec7410060379a111a96
tiger1911 <mehmetatas@yahoo.com>
2020-10-13 22:23:12 +0200

    added one more new line

--- src/test/java/MehmetA.java
+++ src/test/java/MehmetA.java
@@ -3,5 +3,6 @@ public class MehmetA {
     public static void main(String[] args) {
 
         System.out.println("Collaboration in group is super");
+        System.out.println("Group working is difficult");
     }
 }
