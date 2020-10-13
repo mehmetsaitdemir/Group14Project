07717af3314744688816345918b4daec26bf47bb
tiger1911 <mehmetatas@yahoo.com>
2020-10-13 22:33:52 +0200

    added one line

--- src/test/java/MehmetA.java
+++ src/test/java/MehmetA.java
@@ -5,5 +5,6 @@ public class MehmetA {
         System.out.println("Collaboration in group is super");
         //learning as a group
         System.out.println("Just say goodbye");
+        //Where are the people
     }
 }
